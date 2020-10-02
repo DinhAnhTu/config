39a7a964be7b1439bd5a95af1cde7b44ae8bfd23
tudinhanh <tu.dinh@techplus.com.vn>
2020-10-02 11:55:12 +0700

    update

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env sh
+
+## Add this to your wm startup file.
 
 # Terminate already running bar instances
 killall -q polybar
@@ -6,7 +8,6 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar mybar &
-
-echo "Polybar launched..."
+# Launch Polybar
+polybar -c ~/.config/polybar/config.ini top &
+polybar -c ~/.config/polybar/config.ini bottom &
